e2e92141962448ed963986d54bb51284e64be989
ꓤOM🐏 <RomTheSheep@gmail.com>
2024-11-04 18:40:51 +0100

    update

---
+++ Inverse kinematics/Aberracion/losientolosientolosientolosientolosientolosiento.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+@onready var copa_target = $Targets/CopaTarget
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	#copa_target.position = get_global_mouse_position()
